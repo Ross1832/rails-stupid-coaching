b0a91465d6ec4fb4f6515400d4f7e4205dfdf032
Rostyslav Komar <komarostyslav@gmail.com>
2023-05-29 15:36:21 +0300

    Stupid Coaching

---
+++ app/helpers/coah_helper.rb
@@ -0,0 +1,2 @@
+module CoahHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "ask", to: "coah#ask"
+  get "answer", to: "coah#answer"
 end
